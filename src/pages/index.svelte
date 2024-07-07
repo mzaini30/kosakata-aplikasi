a7fb673b1ef73b1d309245639862b988da8e1252
mzaini30 <muhzaini30@gmail.com>
2024-07-07 21:57:19 +0800

    upload

---
+++ src/pages/index.svelte
@@ -0,0 +1,64 @@
+<script>
+  import { acak } from "kumpulan-tools";
+  import kosakata from "../kosakata.json";
+  import santri from "../santri.json";
+
+  let teksFilter = "";
+  let santriSalin = [...santri];
+
+  let listAngka = [];
+  for (let x = 1; x <= kosakata.length; x++) {
+    listAngka = [...listAngka, x];
+  }
+
+  let kosakataApelPagi = acak(listAngka)[0];
+  function filterByKeyword(data, keyword) {
+    if (!keyword) {
+      return data;
+    }
+
+    const keywordChars = keyword.toLowerCase().split("");
+
+    return data.filter((item) => {
+      const nameChars = item.nama.toLowerCase();
+      return keywordChars.every((char) => nameChars.includes(char));
+    });
+  }
+</script>
+
+<div class="container py-4 space-y-4">
+  <h1 class="text-center">Cek Bahasa</h1>
+  <ul class="list-group">
+    <li class="list-group-item text-center active uppercase">
+      Kosakata Apel Pagi
+    </li>
+    <li class="list-group-item">
+      <img src="gambar/{kosakataApelPagi}.PNG" class="block mx-auto" alt="" />
+    </li>
+    <li class="list-group-item text-center">
+      {kosakata[kosakataApelPagi - 1]}
+    </li>
+  </ul>
+  <ul class="list-group">
+    <li class="list-group-item text-center active uppercase">Cek Santri</li>
+    <li class="list-group-item">
+      <div class="input-group">
+        <span class="input-group-text" id="basic-addon1">Filter</span>
+        <input
+          oninput={() => (santriSalin = filterByKeyword(santri, teksFilter))}
+          bind:value={teksFilter}
+          type="search"
+          placeholder="Masukkan Nama Santri"
+          class="form-control"
+        />
+      </div>
+    </li>
+    {#each santriSalin as s}
+      <li class="list-group-item">
+        <a href="#/santri/{s.id}" class="block">
+          {s.nama}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</div>
---
+++ src/pages/santri/[id].svelte
@@ -0,0 +1,45 @@
+<script>
+  export let params = {};
+  import santri from "../../santri.json";
+  import kosakata from "../../kosakata.json";
+  let terpilih = santri.filter((x) => x.id == params.id)[0];
+</script>
+
+<div class="container py-4 space-y-4 text-center">
+  <div class="list-group">
+    <div class="list-group-item uppercase active">{terpilih.nama}</div>
+  </div>
+  <div class="list-group">
+    <div class="list-group-item uppercase active">Mufrodat Dzuhur</div>
+    {#each terpilih.mufrodatDzuhur as d}
+      <div class="list-group-item">
+        <img src="gambar/{d}.PNG" />
+      </div>
+      <div class="list-group-item">
+        {kosakata[d - 1]}
+      </div>
+    {/each}
+  </div>
+  <div class="list-group">
+    <div class="list-group-item uppercase active">Mufrodat Ahad Pagi</div>
+    {#each terpilih.mufrodatAhadPagi as d}
+      <div class="list-group-item">
+        <img src="gambar/{d}.PNG" />
+      </div>
+      <div class="list-group-item">
+        {kosakata[d - 1]}
+      </div>
+    {/each}
+  </div>
+  <div class="list-group">
+    <div class="list-group-item uppercase active">Iqob</div>
+    {#each terpilih.iqob as d}
+      <div class="list-group-item">
+        <img src="gambar/{d}.PNG" />
+      </div>
+      <div class="list-group-item">
+        {kosakata[d - 1]}
+      </div>
+    {/each}
+  </div>
+</div>
